502cef2222fd20b2244794a2ebc34aa0c26adfc7
Hugo Parmentier <hugo.parmentier@efrei.net>
2024-07-18 17:17:04 +0200

    graphspec to do

---
+++ core/src/test/scala/com/scala/core/GraphSpec.scala
@@ -0,0 +1,137 @@
+package com.scala.core
+
+import org.scalatest._
+import zio.json._
+
+class GraphSpec extends UnitSpec {
+  // Test cases for Graph methods
+
+  
+   "DirectedGraph" should "create a new graph with given vertices and edges" in {
+        // Create a new graph following the Graph interface
+    }
+
+   it should "get the set of vertices in the graph" in {
+      
+    }
+
+   it should "set the vertices in the graph" in {
+      
+    }
+
+   it should "clear all vertices in the graph" in {
+      
+    }
+
+   it should "add a vertex to the graph" in {
+      
+    }
+
+   it should "remove a vertex from the graph" in {
+      
+    }
+
+   it should "get the set of edges in the graph" in {
+      
+    }
+
+   it should "set the edges in the graph" in {
+      
+    }
+
+   it should "clear all edges in the graph" in {
+      
+    }
+
+   it should "add an edge to the graph" in {
+      
+    }
+
+   it should "remove an edge from the graph" in {
+      
+    }
+
+   it should "remove a set of edges from the graph" in {
+      
+    }
+
+   it should "get the neighbors edges of a vertex" in {
+      
+    }
+
+   it should "get the neighbors of a vertex" in {
+      
+    }
+
+   it should "get the predecessors edges of a vertex" in {
+      
+    }
+
+   it should "get the predecessors of a vertex" in {
+      
+    }
+
+   it should "get the successors edges of a vertex" in {
+      
+    }
+
+   it should "get the successors of a vertex" in {
+      
+    }
+
+   it should "get the degree of a vertex" in {
+      
+    }
+
+   it should "get the adjacency matrix of the graph" in {
+      
+    }
+
+   it should "get the incidence matrix of the graph" in {
+      
+    }
+
+   it should "get the degree matrix of the graph" in {
+      
+    }
+
+   it should "get the Laplacian matrix of the graph" in {
+      
+    }
+
+   it should "traverse the graph using depth-first search" in {
+      
+    }
+
+   it should "sort the graph using depth-first search" in {
+      
+    }
+
+   it should "traverse the graph using breadth-first search" in {
+      
+    }
+
+   it should "sort the graph using breadth-first search" in {
+      
+    }
+
+   it should "check if the graph has a cycle" in {
+      
+    }
+
+   it should "get the shortest paths between all pairs of vertices using Floyd-Warshall algorithm" in {
+      
+    }
+
+   it should "get the shortest path between two vertices using Floyd-Warshall algorithm" in {
+      
+    }
+
+   it should "get the shortest paths from a source vertex to all other vertices using Dijkstra's algorithm" in {
+      
+    }
+
+   it should "get the shortest path from a source vertex to a destination vertex using Dijkstra's algorithm" in {
+      
+    }
+}
