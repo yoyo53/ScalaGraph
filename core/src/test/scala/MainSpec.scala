c96cf47d2a72701f8282107a598ce4171758725d
Yohan Villiers <yohan.villiers@thekeepers.io>
2024-07-19 23:22:10 +0200

    add tests to main spec

--- core/src/test/scala/MainSpec.scala
+++ core/src/test/scala/MainSpec.scala
@@ -6,6 +6,10 @@ object MainSpec {
       "com.scala.core.VertexSpec",
       "com.scala.core.DirectionSpec",
       "com.scala.core.EdgeSpec",
+      "com.scala.core.DirectedGraphSpec",
+      "com.scala.core.WeightedDirectedGraphSpec",
+      "com.scala.core.UndirectedGraphSpec",
+      "com.scala.core.WeightedUndirectedGraphSpec",
     )
 
     Runner.run(classes.foldLeft(Array("-o", "-fW", "target/test-reports.txt"))((acc, c) => acc ++ Array("-s", c)))
